994e5de41e06b139529355b0e9c054d4b20958c0
Graham Kelly <gkgoat6700@gmail.com>
2023-09-23 21:35:55 -0400

    afero stuff

--- arf.go
+++ arf.go
@@ -5,12 +5,17 @@ import (
 	"io/fs"
 	"os"
 	"path"
+	"strings"
 	"time"
 
 	"github.com/go-git/go-billy/v5"
 	"github.com/hack-pad/hackpadfs"
 	"github.com/spf13/afero"
 )
+
+func ar(x string) string {
+	return strings.TrimPrefix(path.Clean(x), "/")
+}
 
 type AF struct {
 	fs.FS
@@ -20,7 +25,7 @@ type AF struct {
 // it if it already exists. If successful, methods on the returned File can
 // be used for I/O; the associated file descriptor has mode O_RDWR.
 func (b AF) Create(filename string) (afero.File, error) {
-	f, err := hackpadfs.Create(b.FS, filename)
+	f, err := hackpadfs.Create(b.FS, ar(filename))
 	return B{f}, err
 }
 
@@ -28,7 +33,7 @@ func (b AF) Create(filename string) (afero.File, error) {
 // returned file can be used for reading; the associated file descriptor has
 // mode O_RDONLY.
 func (b AF) Open(filename string) (afero.File, error) {
-	f, err := b.FS.Open(filename)
+	f, err := b.FS.Open(ar(filename))
 	return B{f}, err
 }
 
@@ -37,29 +42,29 @@ func (b AF) Open(filename string) (afero.File, error) {
 // perm, (0666 etc.) if applicable. If successful, methods on the returned
 // File can be used for I/O.
 func (b AF) OpenFile(filename string, flag int, perm os.FileMode) (afero.File, error) {
-	f, err := hackpadfs.OpenFile(b.FS, filename, flag, perm)
+	f, err := hackpadfs.OpenFile(b.FS, ar(filename), flag, perm)
 	return B{f}, err
 }
 
 // Stat returns a FileInfo describing the named file.
 func (b AF) Stat(filename string) (os.FileInfo, error) {
-	return hackpadfs.Stat(b.FS, filename)
+	return hackpadfs.Stat(b.FS, ar(filename))
 }
 
 // Rename renames (moves) oldpath to newpath. If newpath already exists and
 // is not a directory, Rename replaces it. OS-specific restrictions may
 // apply when oldpath and newpath are in different directories.
 func (b AF) Rename(oldpath, newpath string) error {
-	return hackpadfs.Rename(b.FS, oldpath, newpath)
+	return hackpadfs.Rename(b.FS, ar(oldpath), ar(newpath))
 }
 
 // Remove removes the named file or directory.
 func (b AF) Remove(filename string) error {
-	return hackpadfs.Remove(b.FS, filename)
+	return hackpadfs.Remove(b.FS, ar(filename))
 }
 
 func (b AF) RemoveAll(filename string) error {
-	return hackpadfs.RemoveAll(b.FS, filename)
+	return hackpadfs.RemoveAll(b.FS, ar(filename))
 }
 
 // Join joins any number of path elements into a single path, adding a
@@ -76,7 +81,7 @@ func (b AF) TempFile(dir, prefix string) (billy.File, error) {
 // ReadDir reads the directory named by dirname and returns a list of
 // directory entries sorted by filename.
 func (b AF) ReadDir(path string) ([]os.FileInfo, error) {
-	x, err := hackpadfs.ReadDir(b.FS, path)
+	x, err := hackpadfs.ReadDir(b.FS, ar(path))
 	if err != nil {
 		return nil, err
 	}
@@ -96,25 +101,25 @@ func (b AF) ReadDir(path string) ([]os.FileInfo, error) {
 // perm are used for all directories that MkdirAll creates. If path is/
 // already a directory, MkdirAll does nothing and returns nil.
 func (b AF) MkdirAll(filename string, perm os.FileMode) error {
-	return hackpadfs.MkdirAll(b.FS, filename, perm)
+	return hackpadfs.MkdirAll(b.FS, ar(filename), perm)
 }
 
 func (b AF) Mkdir(filename string, perm os.FileMode) error {
-	return hackpadfs.Mkdir(b.FS, filename, perm)
+	return hackpadfs.Mkdir(b.FS, ar(filename), perm)
 }
 
 // Lstat returns a FileInfo describing the named file. If the file is a
 // symbolic link, the returned FileInfo describes the symbolic link. Lstat
 // makes no attempt to follow the link.
 func (b AF) Lstat(filename string) (os.FileInfo, error) {
-	return hackpadfs.Lstat(b.FS, filename)
+	return hackpadfs.Lstat(b.FS, ar(filename))
 }
 
 // Symlink creates a symbolic-link from link to target. target may be an
 // absolute or relative path, and need not refer to an existing node.
 // Parent directories of link are created as necessary.
 func (b AF) Symlink(target, link string) error {
-	return hackpadfs.Symlink(b.FS, link, target)
+	return hackpadfs.Symlink(b.FS, ar(link), ar(target))
 }
 
 // Readlink returns the target path of link.
@@ -160,7 +165,7 @@ type FA struct {
 // it if it already exists. If successful, methods on the returned File can
 // be used for I/O; the associated file descriptor has mode O_RDWR.
 func (b FA) Create(filename string) (fs.File, error) {
-	x, err := b.Afero.Create(filename)
+	x, err := b.Afero.Create("/" + filename)
 	return A{x}, err
 }
 
@@ -168,7 +173,7 @@ func (b FA) Create(filename string) (fs.File, error) {
 // returned file can be used for reading; the associated file descriptor has
 // mode O_RDONLY.
 func (b FA) Open(filename string) (fs.File, error) {
-	x, err := b.Afero.Open(filename)
+	x, err := b.Afero.Open("/" + filename)
 	return A{x}, err
 }
 
@@ -177,12 +182,12 @@ func (b FA) Open(filename string) (fs.File, error) {
 // perm, (0666 etc.) if applicable. If successful, methods on the returned
 // File can be used for I/O.
 func (b FA) OpenFile(filename string, flag int, perm os.FileMode) (fs.File, error) {
-	x, err := b.Afero.OpenFile(filename, flag, perm)
+	x, err := b.Afero.OpenFile("/"+filename, flag, perm)
 	return A{x}, err
 }
 
 func (b FA) ReadDir(path string) ([]os.DirEntry, error) {
-	x, err := b.Afero.ReadDir(path)
+	x, err := b.Afero.ReadDir("/" + path)
 	if err != nil {
 		return nil, err
 	}
