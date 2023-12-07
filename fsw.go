73182e031b200cea1e8431071884280b7d9241e4
Graham Kelly <gkgoat6700@gmail.com>
2023-12-07 17:38:38 -0500

    ooh handshake

--- fsw.go
+++ fsw.go
@@ -64,14 +64,14 @@ type FSW struct {
 }
 
 func (f FSW) Open(x string) (hackpadfs.File, error) {
-	y, err := f.Attachment.Open("/"+x, p9.OREAD)
+	y, err := f.Attachment.Open(x, p9.OREAD)
 	var n int64
-	return FSF{File: y, Offset: &n, Path: PathF{Attachment: f.Attachment, Path: "/" + x}}, err
+	return FSF{File: y, Offset: &n, Path: PathF{Attachment: f.Attachment, Path: x}}, err
 }
 func (f FSW) OpenFile(x string, flag int, perm hackpadfs.FileMode) (hackpadfs.File, error) {
-	y, err := f.Attachment.Open("/"+x, fromOSFlags(flag))
+	y, err := f.Attachment.Open(x, fromOSFlags(flag))
 	var n int64
-	return FSF{File: y, Offset: &n, Path: PathF{Attachment: f.Attachment, Path: "/" + x}}, err
+	return FSF{File: y, Offset: &n, Path: PathF{Attachment: f.Attachment, Path: x}}, err
 }
 
 type FSP struct {
